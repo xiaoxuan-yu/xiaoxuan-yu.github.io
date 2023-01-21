cf3a5f31e3483f5b86d632127dfaf6d534d0298e
Youmans Yu <xiaoxuan_yu@pku.edu.cn>
2023-01-21 19:41:42 +0800

    algolia search

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import { searchPlugin } from '@vuepress/plugin-search';
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import { componentsPlugin } from "vuepress-plugin-components";
 
 export default defineUserConfig({
@@ -19,7 +19,16 @@ export default defineUserConfig({
   shouldPrefetch: false,
 
   plugins: [
-    searchPlugin({
+    docsearchPlugin({
+      apiKey: "7f7a2ce9f3f51daa3dbda4fb30c27859",
+      indexName: "blog_youmans",
+      appId: "L5HX5R6H8Z",
+      placeholder: '搜索',
+      translations: {
+        button: {
+          buttonText: "搜索"
+        }
+      }
       // 你的选项
     }),
     //componentsPlugin({
